f576ddf5643b7d650c3154b92bd323ff76bf65f3
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-04-05 15:44:27 -0500

    Get data from api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,21 @@
-export const SET_CITY = 'SET_CITY'
+import transformForecast from './../services/transformForecast';
+import getUrlForecastByCity from './../services/getUrlForecastByCity';
 
-export const setCity = value => ({ type: SET_CITY, value });
+export const SET_CITY = 'SET_CITY';
+export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
+
+const setCity = value => ({ type: SET_CITY, value });
+const setForecastData = value => ({ type: SET_FORECAST_DATA, value });
+
+export const setSelectedCity = value => {
+    return dispatch => {
+        dispatch(setCity(value));
+        const api_forecast = getUrlForecastByCity(value)
+        return fetch(api_forecast).then( 
+            resolve => (resolve.json())
+        ).then(data =>{
+            const forecastData = transformForecast(data);
+            dispatch(setForecastData({city:value, forecastData}))
+        });
+    }
+};
