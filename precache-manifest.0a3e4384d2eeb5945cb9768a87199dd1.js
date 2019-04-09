3c2eedfda410a98dc7e0eb1b92af8939b42cc131
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-04-09 17:00:12 -0500

    Updates

---
+++ precache-manifest.0a3e4384d2eeb5945cb9768a87199dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "48aba5ba460a76c28306",
+    "url": "/weather-app/static/js/main.48aba5ba.chunk.js"
+  },
+  {
+    "revision": "ec8a2d8332f5a1cfa1c4",
+    "url": "/weather-app/static/js/1.ec8a2d83.chunk.js"
+  },
+  {
+    "revision": "48aba5ba460a76c28306",
+    "url": "/weather-app/static/css/main.3e2aab7d.chunk.css"
+  },
+  {
+    "revision": "ec8a2d8332f5a1cfa1c4",
+    "url": "/weather-app/static/css/1.18a5b682.chunk.css"
+  },
+  {
+    "revision": "d05f73c164a99843fbc1348fb60c2fda",
+    "url": "/weather-app/index.html"
+  }
+];
